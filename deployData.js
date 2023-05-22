31d56928e523e76d3605c2290088f0b21f2c2c29
chendra <shendolz201@gmail.com>
2023-05-22 20:26:29 +0700

    first commit

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Chendra Muhammad Azhari Sofyan'; // TODO: replace this
+
+const studentId = 'FE5394940'; // TODO: replace this
+
+const NetlifyDeployUrl = 'https://libraryapp-chendramuhammad.netlify.app/client/index.html'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
